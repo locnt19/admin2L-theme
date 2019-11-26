20c5022fe0bacf36b5a6e2d642e64699f1281668
Loc Thanh <locnt19@gmail.com>
2019-11-26 09:07:16 +0700

    ✨ Import ChartJS to project

--- src/partials/_libraries-ui.js
+++ src/partials/_libraries-ui.js
@@ -4,10 +4,12 @@ module.exports = {
 	],
 	"styles": [
 		"bower_components/metro/build/css/metro-all.min.css",
+		"bower_components/chart.js/dist/Chart.min.css",
 		"bower_components/flag-icon-css/css/flag-icon.min.css",
 		"bower_components/mdi/css/materialdesignicons.min.css"
 	],
 	"scripts": [
-		"bower_components/metro/build/js/metro.min.js"
-	]
+		"bower_components/metro/build/js/metro.min.js",
+		"bower_components/chart.js/dist/Chart.min.js",
+	],
 }
